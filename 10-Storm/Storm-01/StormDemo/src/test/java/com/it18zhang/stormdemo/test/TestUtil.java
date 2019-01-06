f196183d19660159645ea82914fd89eb0282d497
lianliang <lianliang2014@126.com>
2019-01-06 21:51:59 +0800

    commit bigdata datum.

---
+++ 10-Storm/Storm-01/StormDemo/src/test/java/com/it18zhang/stormdemo/test/TestUtil.java
@@ -0,0 +1,14 @@
+package com.it18zhang.stormdemo.test;
+
+import com.it18zhang.stormdemo.util.Util;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/4/1.
+ */
+public class TestUtil {
+    @Test
+    public void test1(){
+        System.out.println(Util.info(this,"test1(hello world)"));
+    }
+}
